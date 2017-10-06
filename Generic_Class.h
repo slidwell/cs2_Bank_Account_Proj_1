7e9be215b2bc31934ffae31e49172d7055aa13fc
slidwell <32403051+slidwell@users.noreply.github.com>
2017-10-06 00:21:39 -0400

    Quick Example of some code

---
+++ Generic_Class.h
@@ -0,0 +1,18 @@
+#ifndef Generic_Class//header for the generic
+#define Generic_Class
+
+class Generic
+{
+private:
+	virtual double balance;
+	int num_deposits_this_month;
+	int num_withdrawals;
+	double annual_interest;
+	double monthly_service_charge;
+public:
+
+	//quick example of something
+		
+};
+
+#endif
